94018c21fa9bdfc4158dfe64c639986385b93aae
salvadorMartinezCervantes <juan_martinez@tesh.edu.mx>
2020-04-13 03:12:26 -0500

    my secod commit

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 var btnMenu = document.getElementById('btn-menu');
 var nav = document.getElementById('nav');
-btnMenu.addEventListener('click', function(){
-  nav.classList.toggle('mostrar');
-});
+btnMenu.addEventListener('click',function(){
+   nav.classList.toggle('muestrate');
+})
