cd83bdb0b6f81d9140aaee527027d7374e72775a
salvadorMartinezCervantes <juan_martinez@tesh.edu.mx>
2020-05-11 15:05:03 -0500

    navbar cambio

---
+++ netProject/Models/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace netProject.Models
+{
+    public class Prueba
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Compania { get; set; }
+        public int Empleados { get; set; }
+
+    }
+}
---
+++ netProject/PruebasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using netProject.Models;
+using netProject.Migrations;
+
+namespace netProject
+{
+    public class PruebasController : Controller
+    {
+        private EjemploContext db = new EjemploContext();
+
+        // GET: Pruebas
+        public ActionResult Index()
+        {
+            return View(db.Pruebas.ToList());
+        }
+
+        // GET: Pruebas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prueba prueba = db.Pruebas.Find(id);
+            if (prueba == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prueba);
+        }
+
+        // GET: Pruebas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pruebas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Compania,Empleados")] Prueba prueba)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pruebas.Add(prueba);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(prueba);
+        }
+
+        // GET: Pruebas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prueba prueba = db.Pruebas.Find(id);
+            if (prueba == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prueba);
+        }
+
+        // POST: Pruebas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Compania,Empleados")] Prueba prueba)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(prueba).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(prueba);
+        }
+
+        // GET: Pruebas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prueba prueba = db.Pruebas.Find(id);
+            if (prueba == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prueba);
+        }
+
+        // POST: Pruebas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Prueba prueba = db.Pruebas.Find(id);
+            db.Pruebas.Remove(prueba);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
